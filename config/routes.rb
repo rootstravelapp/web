f0627e5a66c415a55923167237f5677729bd2833
bhuwan-arora <bhuwanarora67@gmail.com>
2017-09-26 12:08:23 +0530

    subdomain career

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root "website#index"
-	constraints(AdminSubdomain) do
+	constraints(CareerSubdomain) do
 		match "/"          			=> "website#career",                :via => [:get]
 	end
 end
