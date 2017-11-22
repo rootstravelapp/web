ea6e89afc8ac47fc6d8bd169eabc52e345bfd8ad
bhuwan-arora <bhuwanarora67@gmail.com>
2017-11-22 17:17:18 +0530

    home page

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -1,5 +1,14 @@
 class CareerController < ApplicationController
 
 	def index
+		render :layout => "careers"
+	end
+
+	def teams
+		render :layout => "careers"
+	end
+
+	def rootship
+		render :layout => "careers"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root "website#index"
 	get "/careers" => "career#index"
+	get "/careers/teams" => "career#teams"
+	get "/careers/teams/rootship" => "career#rootship"
+
 	get "/blog" => "website#blog"
 	get "/privacy" => "website#privacy"
 	get "/terms" => "website#terms"
