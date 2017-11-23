c0a7c5972e61ee191c7fbbcf0000a813bc6fd7de
bhuwan-arora <bhuwanarora67@gmail.com>
2017-11-23 20:51:45 +0530

    JD

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -27,7 +27,7 @@ class CareerController < ApplicationController
 		remote_internships_ids = ["2","4","5","6"]
 		is_remote = remote_internships_ids.include? @internship_id
 		unless is_remote
-			@who_can_apply_notes =  ["are available <b>full time in Gokarna</b> for the duration of internship.", "can handle the expenses of their <b>lodging and food themselves</b>."]
+			@who_can_apply_notes =  ["are available full time in Gokarna for the duration of internship.", "can handle the expenses of their lodging and food themselves."]
 		end
 
 		
@@ -35,16 +35,16 @@ class CareerController < ApplicationController
 		when "1"
 			# 
 			@internship_name = "Business Development"
-			@looking_for_points = ["Connect and <b>communicate with different local businesses</b>.", "Ideate and <b>suggest different strategies to get local businesses on board</b>.", "<b>Collect data efficiently from different Businesses</b>.", "<b>Manage different strategic events on Roots Calendar</b>.", "Act as a <b>Customer Care representative for local Businesses</b>."]
+			@looking_for_points = ["Connect and communicate with different local businesses.", "Ideate and suggest different strategies to get local businesses on board.", "Collect data efficiently from different Businesses.", "Manage different strategic events on Roots Calendar.", "Act as a Customer Care representative for local Businesses."]
 		when "2"
 			@internship_name = "Content Researcher"
-			@looking_for_points = ["Help in <b>Data Cleaning and Curation</b>.", "<b>Work closely with the Tech Team</b> in building an AI.", "<b>Read, research, analyse and curate content from Tourism based articles and blogs.</b>", "<b>Help in the data entry</b> for Roots."]
+			@looking_for_points = ["Help in Data Cleaning and Curation.", "Work closely with the Tech Team in building an AI.", "Read, research, analyse and curate content from Tourism based articles and blogs.", "Help in the data entry for Roots."]
 		when "3"
 			@internship_name = "Offline Marketing"
-			@looking_for_points = ["Organise and manage <b>offline marketing events</b> in Gokarna", "Carry out the <b>Gokarna Treasure Hunt</b> in the last week of December.", "<b>Posterise Gokarna</b> and would help Roots in getting more users on board", "Directly handle and impact the <b>user onboarding strategies</b>.", "<b>Be the Roots evangelist</b> and would promote Roots among the Tourists in Gokarna."]
+			@looking_for_points = ["Organise and manage offline marketing events in Gokarna", "Carry out the Gokarna Treasure Hunt in the last week of December.", "Posterise Gokarna and would help Roots in getting more users on board", "Directly handle and impact the user onboarding strategies.", "Be the Roots evangelist and would promote Roots among the Tourists in Gokarna."]
 		when "4"
 			@internship_name = "Online Marketing"
-			@looking_for_points = ["Create and organise <b>marketing campaigns on the social media</b>.", "<b>Curate content</b> to build an online brand presence for Roots.", "Use <b>Guerilla Marketing Strategies</b> to get more relevant users on board.", "Manage <b>Facebook, Instagram, Twitter and Google Plus</b> accounts for Roots."]
+			@looking_for_points = ["Create and organise marketing campaigns on the social media.", "Curate content to build an online brand presence for Roots.", "Use Guerilla Marketing Strategies to get more relevant users on board.", "Manage Facebook, Instagram, Twitter and Google Plus accounts for Roots."]
 		end
 
 		@title = @internship_name + SeoContent::Title::Internship
