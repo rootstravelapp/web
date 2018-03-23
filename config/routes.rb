56e7ebc13ccc6d7ad4edaf110629247c857234e6
bhuwan-arora <bhuwanarora67@gmail.com>
2018-03-23 19:57:39 +0530

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
 	get "/press" => "website#press"
 	get '/businesses' => 'website#businesses'
 	get '/menu' => 'website#menu'
-	get '/ico' => 'website#ico'
+	get '/hidden' => 'website#ico'
 end
