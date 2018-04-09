8aecd5d0252291a27ca4c229428273d1aad5ba0c
bhuwan-arora <bhuwanarora67@gmail.com>
2018-04-09 16:44:35 +0530

    roots content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root "website#coming_soon"
+	root "website#ico"
 
 	get "/careers" => "career#index"
 	get "/careers/teams" => "career#teams"
